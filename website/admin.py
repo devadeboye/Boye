f01b0e3710e04bf4debcab72fdf9cf5dec0114c0
p1st1s <emmanueladeboye2017@gmail.com>
2019-09-20 07:09:30 +0100

    - add url-map, view for the homepage - add base_generic html template for the page - add the index.html page for the homepage - register path to static files to django paths - add fontawesome fonts and w3 css for better page rendering

--- website/admin.py
+++ website/admin.py
@@ -8,4 +8,9 @@ admin.site.register(Category)
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
+    # list the fields to be displayed in adminsite and how
+    # to show them
     list_display = ('project_name', 'category', 'image', 'pub_date')
+
+    # add a filter to filter projects in the adminsite
+    list_filter = ('category',)
--- website/views.py
+++ website/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from website.models import Category, Project
+
+def index(request):
+    """ view function for homepage sites """
+
+    context={
+        
+    }
+    # render the page in the html template index.html
+    return render(request, 'index.html', context=context)
